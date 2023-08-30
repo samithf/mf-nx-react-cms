a7cb256bbef4ee14bd98262f1e4072928e35759f
Samith Dilhara <hsamithdilhara@gmail.com>
2023-08-30 23:30:23 +0530

    changed routes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,24 @@
 import { Typography } from '@mui/material';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { CreateBanner } from './create-banner';
 import NxWelcome from './nx-welcome';
+import PlannedOutage from './planned-outage/planned-outage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>hello cms</div>,
+  },
+  {
+    path: '/create-banner',
+    element: <CreateBanner />,
+  },
+]);
 
 export function App() {
   return (
     <div>
-      <Typography>Welcome to NSER!</Typography>
+      <RouterProvider router={router} />
     </div>
   );
 }
